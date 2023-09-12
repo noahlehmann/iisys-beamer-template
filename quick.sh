92341707456f1a4d82b46ee58099df1e9400f132
noahlehmann <noah.lehmann@protonmail.com>
2023-09-12 11:18:51 +0200

    main: init

---
+++ quick.sh
@@ -0,0 +1,3 @@
+find . \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o -iname '*.lof' -o -iname '*.run.xml' -o -iname '*.fdb_latexmk' -o -iname '*.fls' -o -iname '*.bbl' -o -iname '*.bcf' -o -iname '*.blg' -o -iname '*.lot' \) -exec rm -R {} +
+docker run --rm -t --user="$(id -u):$(id -g)" --net=none -v "$(pwd):/tmp" leplusorg/latex:1.0 latexmk -outdir=/tmp -pdf /tmp/Masterarbeit.tex
+find . \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o -iname '*.lof' -o -iname '*.run.xml' -o -iname '*.fdb_latexmk' -o -iname '*.fls' -o -iname '*.bbl' -o -iname '*.bcf' -o -iname '*.blg' -o -iname '*.lot' \) -exec rm -R {} +
